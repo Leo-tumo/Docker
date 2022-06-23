4dfc604bcce367ec07823add7e79f0f94282fbe1
Leo-tumo <leo_tum@mail.ru>
2022-06-23 19:52:40 +0400

    inception

---
+++ node-app/index.mjs
@@ -0,0 +1,7 @@
+import fs from 'fs'
+
+fs.appendFile('my-file.txt', 'File created Node.js', (err) => {
+	if (err) throw err
+ 	console.log('File saved!')
+})
+setTimeout(() => console.log('End!'), 10000) 
---
+++ python-app/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('Welcome to super calendar\n')
+
+year = int(input('Նշեք տարին։ '))
+month = int(input('Նշեք որևէ ամիս։ '))
+
+print(calendar.month(year, month))
+
+print('Հաջողություն!')
